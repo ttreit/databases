5851135c8e7598d2f9a19faa2fa7e79ec101c81e
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-19 14:57:18 -0700

    Returns JSON object - need to separate db query out

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,9 +1,24 @@
 var models = require('../models');
+var db = require('../db');
+
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
-    post: function (req, res) {} // a function which handles posting a message to the database
+    get: function (req, res) {
+      db.query('SELECT * FROM messages',
+        function (error, results, fields) {
+          if (error) { throw error; }
+          res.end(JSON.stringify(results));
+        });
+    },
+    // get: function (req, res) {
+    //  return res.end(JSON.stringify(models.messages.get()));
+    // }, // a function which handles a get request for all messages
+
+    post: function (req, res) {
+      console.log('****REQDATA**** ', req.body);
+
+    } // a function which handles posting a message to the database
   },
 
   users: {
