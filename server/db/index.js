d2b0aa57ebf44f181e96beab25d1950066a9aace
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-19 13:11:49 -0700

    Add connection to db

--- server/db/index.js
+++ server/db/index.js
@@ -1,5 +1,21 @@
 var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+  host:'localhost',
+  user: 'student',
+  password: 'student',
+  database: 'chat'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM users', (err, rows, fields) => {
+  if (err) {throw err;}
+  console.log('rows: ', rows);
+  //console.log('fields: ', fields);
+});
+
+
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
