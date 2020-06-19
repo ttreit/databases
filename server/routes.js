32aea1f97fff61d4889d58ccbea1dd37d53681a6
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-19 13:08:57 -0700

    Add comment

--- server/routes.js
+++ server/routes.js
@@ -2,6 +2,8 @@ var controller = require('./controllers');
 var router = require('express').Router();
 
 //Connect controller methods to their corresponding routes
+
+//classes/what's below
 router.get('/messages', controller.messages.get);
 
 router.post('/messages', controller.messages.post);
