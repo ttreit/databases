0eb316d1f0ba24e330afa57a14f4f75ca21e0d27
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-19 19:45:50 -0700

    Add post message functionality

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -10,15 +10,21 @@ module.exports = {
 
 
     post: function (req, res) {
-      console.log('****REQDATA**** ', req.body);
-
+      models.messages.post(req.body);
     } // a function which handles posting a message to the database
   },
 
   users: {
     // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    get: function (req, res) {
+      models.users.get((results) => {
+        res.end(JSON.stringify(results));
+      });
+    },
+    post: function (req, res) {
+      models.users.post(req.body);
+      res.end();
+    }
   }
 };
 
--- server/models/index.js
+++ server/models/index.js
@@ -10,13 +10,43 @@ module.exports = {
       });
 
     },
-    post: function () {} // a function which can be used to insert a message into the database
+    post: function (body) {
+      db.query(`SELECT id FROM users WHERE name = '${body.username}';`, (err, results, fields) => {
+        let user_id = (results);
+        user_id = user_id[0]['id'];
+        console.log('first callback: ', user_id);
+        db.query(`SELECT id FROM rooms WHERE name = '${body.roomname}';`, (err, results, fields) => {
+          let room_id = (results);
+          room_id = room_id[0]['id'];
+          console.log('second callback: ', room_id, user_id);
+          db.query(`INSERT INTO messages (text, user_id, room_id) VALUES ('${body.message}', ${user_id}, ${room_id});`);
+        });
+      });
+    }
+
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      db.query('SELECT * FROM users', (err, results, fields) => {
+        if (err) { throw err; }
+        callback(results);
+      })
+
+    },
+    post: function (body) {
+      db.query(`INSERT INTO users (name) VALUES ('${body.username}');`)
+      //console.log('***** username: ', body.username);
+    }
   }
 };
 
+// db.query(`SELECT id FROM users WHERE name = '${body.username}';`, (err, results, fields) => {
+//   let user_id = results;
+//   (body, user_id) => db.query(`SELECT id FROM rooms WHERE name = '${body.roomname}';`, (err, results, fields) => {
+//     let room_id = results;
+//     (body, user_id, )
+//   })
+// });
+// //db.query (user_id, )
