f2bf40d15d93113b9973133f3e4e83173f8b88f1
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-19 17:00:45 -0700

    Create response for GET /classes/messages

--- server/models/index.js
+++ server/models/index.js
@@ -2,12 +2,12 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {
-      //connect to DB?? Or already connected?
-      //get all messages SELECT * FROM messages
-      const data = 'check 1 check';
-      console.log('test data: ', data);
-      return data;
+    get: function (callback) {
+      db.query('SELECT * FROM messages', (err, results, fields) => {
+        //query finishes and then continue through this callback...
+        if (err) { throw err; }
+        callback(results);
+      });
 
     },
     post: function () {} // a function which can be used to insert a message into the database
