8334af07923ad9688bc88c7d46efc10dd4890243
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-19 17:05:03 -0700

    Clean up comments

--- server/db/index.js
+++ server/db/index.js
@@ -15,9 +15,4 @@ connection.query('SELECT * FROM users', (err, rows, fields) => {
   //console.log('fields: ', fields);
 });
 
-
-// Create a database connection and export it from this file.
-// You will need to connect with the user "root", no password,
-// and to the database "chat".
-
 module.exports = connection;
