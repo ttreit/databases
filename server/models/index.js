10813928ad61a8aa4ff4f82a05207ef8dc36feb9
ttreit <14933277+ttreit@users.noreply.github.com>
2020-06-20 21:12:21 -0700

    Refactor to use sequelize - get users

--- server/models/index.js
+++ server/models/index.js
@@ -3,19 +3,19 @@ var Sequelize = require('sequelize');
 
 db.authenticate()
   .then(() => console.log('Database connected...'))
-  .catch(err => console.log('Error: ', err))
+  .catch(err => console.log('Error: ', err));
 
 module.exports = {
   messages: {
     get: function (callback) {
-      db.query(`SELECT users.username, rooms.name, messages.text FROM users INNER JOIN messages ON users.id = messages.user_id INNER JOIN rooms ON rooms.id = messages.room_id;`)
-      .then((message) => {
-      let results;
-        [results] = message;
-        callback(results);
-      }).catch((message) => {
-        console.log('catch******: ', message)
-      })
+      db.query('SELECT users.username, rooms.name, messages.text FROM users INNER JOIN messages ON users.id = messages.user_id INNER JOIN rooms ON rooms.id = messages.room_id;')
+        .then((message) => {
+          let results;
+          [results] = message;
+          callback(results);
+        }).catch((message) => {
+          console.log('catch******: ', message);
+        });
     },
     post: function (body, callback) {
       db.query(`SELECT id FROM users WHERE username = '${body.username}';`, (err, results, fields) => {
@@ -35,28 +35,24 @@ module.exports = {
   },
 
   users: {
-    // Ditto as above.
     get: function (callback) {
-      db.query('SELECT * FROM users', (err, results, fields) => {
-        if (err) { throw err; }
-        callback(results);
-      });
-
+      db.query('SELECT * FROM users')
+        .then((data) => {
+          let results;
+          [results] = data;
+          callback(results);
+        }).catch((error) => {
+          console.log('Caught an error: ', error);
+        });
     },
     post: function (body, callback) {
-      db.query(`INSERT INTO users (username) VALUES ('${body.username}');`, (err, results, fields) => {
-        callback();
-      });
+      db.query(`INSERT INTO users (username) VALUES ('${body.username}')`);
     }
   },
 
   test: {
     get: function (callback) {
-      db.query('SELECT * FROM test', (err, results, fields) => {
-        if (err) { throw err; }
-        console.log('***** test results: ', results);
-        callback(results);
-      });
+      db.query('SELECT * FROM test');
     }
   }
 };
